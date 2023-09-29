1f0a45811ab3078501834e3376db7ad602e90be9
Anton Antich <aantich@gmail.com>
2023-09-29 23:48:31 +0200

    added spaceship folder

---
+++ threetest/imports/games/lastspaceship/api/Spaceship.ts
@@ -0,0 +1,31 @@
+/**
+ * Survival / Trading / Exploration in space with focus on radar gameplay inspired by "the last battleship"
+ * 
+ * The idea is - have a spaceship, moving from system to system,
+ * tactical map is all based on various radar - active / passive / transponders etc.
+ * 
+ * Then combat / trading / missions.
+ * 
+ * Upgrade of the systems etc as usual.
+ * 
+ * TYPES OF DAMAGE
+ * 
+ * - Breaking molecular bonds:
+ *      - Mechanical / Kinetic
+ *      - Photons (lasers)
+ *      - Electrically charged particles (up to certain energies?)
+ * 
+ * - Breaking atoms apart - so not just molecular bonds, but kicking electrons away from nuclei. Basically, as previous but higher energies.
+ * 
+ * - Breaking nuclei apart - nuclear fission:
+ *      - ECP beams of very high energies
+ *      - Neutron beams (how do we generate?)
+ *      - Can very energetic gamma photons do this?
+ * 
+ * - Breaking apart protons and neutrons - strong force:
+ *      - Quark-gluon plasma beams - cuts through any matter just like that, huge energies. Any protection?? Some custom Quark configurations to interact with these beams??
+ * 
+ * - Gravity: space curving
+ * 
+ * - Higgs bozon beams: changing masses of particles with some crazy results
+ */
