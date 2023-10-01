857a2ea40a669047e0ff8f0f7702b861e4ddc0b0
Anton Antich <aantich@gmail.com>
2023-10-01 22:16:03 +0200

    added overflow in terminal

--- threetest/imports/games/antares/ui/Terminal.tsx
+++ threetest/imports/games/antares/ui/Terminal.tsx
@@ -4,16 +4,52 @@ const ttest = `Hello world, (c) 1987
 terminal asceta v2.5
 
 Documentation: help
+
+wafs
+asf
+fas
+afs
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asfas
+fa
+sfa
+sf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
+asf
 `
 
 
 export const Terminal = ()=> {
 
     return (
-        <pre className="terminal"><output>
+        <pre className="terminal" style={{overflowY: "auto"}}><output>
           {ttest}
 </output><br/>
-$><input type="text" className="term-input"></input>
+$<input type="text" className="term-input"></input>
 </pre>
     )
 }
