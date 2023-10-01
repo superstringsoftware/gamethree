ba29302a9fcf9542e236275e78236bfeb0dbec96
Anton Antich <aantich@gmail.com>
2023-10-01 22:32:00 +0200

    basic command processor for the terminal setup

--- threetest/imports/games/antares/ui/Terminal.tsx
+++ threetest/imports/games/antares/ui/Terminal.tsx
@@ -1,55 +1,50 @@
 import React from 'react'
+import { useState } from 'react'
 
 const ttest = `Hello world, (c) 1987
 terminal asceta v2.5
 
 Documentation: help
 
-wafs
-asf
-fas
-afs
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asfas
-fa
-sfa
-sf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
-asf
 `
 
 
 export const Terminal = ()=> {
 
+    const [term, setTerm] = useState(ttest)
+    const [inp, setInp] = useState("")
+
+    const kp = (e) => {
+        //console.log(e.code)
+        if (e.code === "Enter") {
+            e.preventDefault()
+            setTerm(term+"\n"+inp)
+            switch (inp) {
+                case "help": 
+                    setTerm(term+inp+`
+This is the terminal system ascetos v2.5. 
+Available commands:
+
+help: this message
+`); break;
+                default: setTerm(term+inp+`
+Unknown command. Try "help" for help.
+
+`); break;
+            }
+            setInp("")
+            
+        }
+    }
+
     return (
         <pre className="terminal" style={{overflowY: "auto"}}><output>
-          {ttest}
+          {term}
 </output><br/>
-$<input type="text" className="term-input"></input>
+$<input type="text" className="term-input" 
+value={inp}
+onChange={(e)=>setInp(e.target.value)}
+onKeyDown={(e)=>kp(e)}></input>
 </pre>
     )
 }
